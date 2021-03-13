7e598cfdced52357cd3f86b14614aca2ca4b8191
Sia2602 <danilojose26@hotmail.com>
2021-03-13 15:22:56 -0300

    Adicionando Modificacoes

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,40 @@
         target: "#sideNav",
     });
 })(jQuery); // End of use strict
+
+//mudando imagem botao AR para portugues
+function imgBR()
+{
+  var img = document.getElementById("dropDownCurrent");
+  img.src="assets/img/brazil.svg";
+}
+
+//mudando imagem botao AR para portugues
+function imgEUA()
+{
+  var img = document.getElementById("dropDownCurrent");
+  img.src="assets/img/eua.svg"
+}
+
+//mudando idoma para portugues
+function changeLanguage_BR()
+{  
+  document.getElementById('home01').innerHTML = "Inicio";
+  document.getElementById('tecnology01').innerHTML = "Tecnologia";
+  document.getElementById('aplications01').innerHTML = "Aplicações";
+  document.getElementById('parceiros01').innerHTML = "Parceiros";
+  document.getElementById('equipe01').innerHTML = "Equipe";
+  document.getElementById('contato01').innerHTML = "Contato";  
+}
+
+//mudando idoma para ingles
+function changeLanguage_EUA()
+{  
+  document.getElementById('home01').innerHTML = "Home";  
+  document.getElementById('tecnology01').innerHTML = "Tecnology";
+  document.getElementById('aplications01').innerHTML = "Aplications";
+  document.getElementById('parceiros01').innerHTML = "Partners";
+  document.getElementById('equipe01').innerHTML = "Team";
+  document.getElementById('contato01').innerHTML = "Contact";
+}
+
